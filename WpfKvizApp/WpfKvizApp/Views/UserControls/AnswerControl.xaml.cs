6f0fcebfb90ffddd3210f3dee89ae2d885fbae2d
Szanto Benjamin <szantobenjamin97@gmail.com>
2024-01-08 22:03:57 +0100

    mai óra

--- WpfKvizApp/WpfKvizApp/Views/UserControls/AnswerControl.xaml.cs
+++ WpfKvizApp/WpfKvizApp/Views/UserControls/AnswerControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfKvizApp.Models;
 
 namespace WpfKvizApp.Views.UserControls
 {
@@ -20,14 +21,39 @@ namespace WpfKvizApp.Views.UserControls
     /// </summary>
     public partial class AnswerControl : UserControl
     {
-        public AnswerControl()
+        Answer answer;
+        public AnswerControl(Answer answer, string groupName)
         {
             InitializeComponent();
+            this.answer = answer;
+            SetAnswerData(answer);
+            SetGroupName(groupName);
+        }
+
+        public bool IsTheAnswerCorrect()
+        {
+            return radioButtonControl.IsChecked == true && answer.Validity == AnswerValidity.Correct;
+        }
+
+        public bool IsChecked()
+        {
+            return radioButtonControl.IsChecked == true;
+        }
+
+        private void SetGroupName(string groupName)
+        {
+            radioButtonControl.GroupName = groupName;
+        }
+
+        private void SetAnswerData(Answer answer)
+        {
+            textBlockControl.Text = answer.Text;
+            imageControl.Source = new BitmapImage(new Uri(answer.ImageSource));
         }
 
         private void StackPanel_MouseUp(object sender, MouseButtonEventArgs e)
         {
-
+            radioButtonControl.IsChecked = true;
         }
     }
 }
